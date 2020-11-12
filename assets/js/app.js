10b3678d10414543b738321e0faf7d699f52a715
Kuba Kuźma <kuba@kubakuzma.com>
2020-11-12 12:33:20 +0100

    added cname

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+(function () {
+  jQuery(function ($) {
+    $("#carousel").on("slid.bs.carousel", function (transition) {
+      $("a[data-target='#carousel']").removeClass("text-dark").addClass("text-muted");
+      $("a[data-target='#carousel'][data-slide-to=" + transition.to + "]").removeClass("text-muted").addClass("text-dark");
+    });
+  });
+})();
