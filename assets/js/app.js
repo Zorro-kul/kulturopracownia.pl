9c721aac5144b90be0845ee11c129d2c3db63a93
Kuba Kuźma <kuba@kubakuzma.com>
2020-11-18 22:37:23 +0100

    added tooltips

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,5 +4,7 @@
       $("a[data-target='#carousel']").removeClass("text-dark").addClass("text-muted");
       $("a[data-target='#carousel'][data-slide-to=" + transition.to + "]").removeClass("text-muted").addClass("text-dark");
     });
+    $("[data-toggle=popover]").popover();
+    $("[data-toggle=tooltip]").tooltip();
   });
 })();
